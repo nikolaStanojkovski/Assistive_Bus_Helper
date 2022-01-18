2ed71479a0f9732ce80a73ba73779972fa240977
Nikola Stanojkovski <65570806+nikolaStanojkovski@users.noreply.github.com>
2022-01-18 20:18:13 +0100

    [no-issue] Add text-to-speech model in use.

---
+++ andorid_app/app/fastspeech2/prepare_align.py
@@ -0,0 +1,18 @@
+import argparse
+
+import yaml
+
+from fastspeech2.preprocessor import ljspeech
+
+
+def main(config):
+    ljspeech.prepare_align(config)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", type=str, help="path to preprocess.yaml")
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config, "r"), Loader=yaml.FullLoader)
+    main(config)
